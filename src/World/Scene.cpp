1e835b83ee50c25032b9a87f3fc6d09fa9aebe7f
Yang Qi <1578434841@qq.com>
2020-03-24 17:17:50 +0800

    add scene class

--- src/World/Scene.cpp
+++ src/World/Scene.cpp
@@ -1,16 +1,55 @@
 #include "Scene.h"
 
 namespace Game {
-
-	void Scene::Render() 
+	
+	Scene::Scene(const HWND AppHwnd, const UINT width, const UINT height)
+		: m_SwapChain(AppHwnd, width, height), m_Camera(width, height)
 	{
-		// TODO multi threading 
-		for (auto const& g_obj : m_ObjList)
-		{
-			
-
-
-		}
+		m_SwapChain.Initialize(GraphicsCommandManager.GetCommadnQueue());
+		m_Camera.CreateCBV();
 	}
 
+	void Scene::Render()
+	{
+		// TODO multi threading here
+		Graphic::CommandList ThreadCommandList;
+		GraphicsCommandManager.InitCommandList(&ThreadCommandList);
+		ThreadCommandList.SetSwapChain(m_SwapChain);
+
+		for (auto const& g_obj : m_ObjList)
+		{
+			g_obj->RecordCommand(ThreadCommandList);
+
+			m_Camera.UseCamera(ThreadCommandList, g_obj->GetTransform());
+			ThreadCommandList.ResourceBarrier(m_SwapChain, D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET);
+			g_obj->Draw(ThreadCommandList);
+			ThreadCommandList.ResourceBarrier(m_SwapChain, D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT);
+		}
+
+		GraphicsCommandManager.ExecuteCommandList(&ThreadCommandList);
+	}
+
+	void Scene::BeginRender() 
+	{
+		Graphic::CommandList mainCommandList;
+		const float clearColor[] = { 0.0f, 0.2f, 0.4f, 1.0f };
+		GraphicsCommandManager.InitCommandList(&mainCommandList);
+
+		mainCommandList.SetSwapChain(m_SwapChain);
+		mainCommandList.ResourceBarrier(m_SwapChain, D3D12_RESOURCE_STATE_PRESENT, D3D12_RESOURCE_STATE_RENDER_TARGET);
+		mainCommandList.ClearSwapChain(m_SwapChain, clearColor);
+		mainCommandList.ResourceBarrier(m_SwapChain, D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT);
+	}
+
+	void Scene::EndRender()
+	{
+		GraphicsCommandManager.End();
+		m_SwapChain.Present();
+	}
+
+	void Scene::Update() 
+	{
+		// TODO DO NOTHING FOR NOW
+
+	}
 }
--- src/World/Scene.h
+++ src/World/Scene.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "GraphicCore.h"
 #include "CommandList.h"
+#include "CommandQueue.h"
 #include "GObject.h"
 #include "Camera.h"
 #include "Light.h"
@@ -8,18 +10,26 @@
 namespace Game 
 {
 
-	class Scene 
+	class Scene
 	{
 	public:
+		Scene(const HWND m_appHwnd, const UINT width, const UINT height);
+
 		virtual void Render();
 
 		virtual void Update();
 
 	protected:
+
+		void BeginRender();
+		void EndRender();
+
 		// TODO need change
-		Camera m_Camera;
+		ProjectiveCamera m_Camera;
 		std::vector<ptrLight> m_LightList;
 		std::vector<ptrGobject> m_ObjList;
+
+		Graphic::SwapChain m_SwapChain;
 	};
 
 }
